f03ab4851717fe55859eb4cbad186164789a28b1
James McCarron <mccarron.j@gmail.com>
2021-03-18 12:28:05 +1100

    Blocks

---
+++ Ruby_Basics/assignment_operators.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+a = 10
+
+# a = a + 5
+a += 5
+
+p a
+
+b = 100
+
+# b = b - 50
+b -= 50
+p b
+
+# Introduction to Blocks
+# 3.times { puts 'James is awesome!' }
--- Ruby_Basics/numbers_and_booleans.rb
+++ Ruby_Basics/numbers_and_booleans.rb
@@ -97,3 +97,8 @@ p 10.9.to_i.class
 
 p 10.5.floor
 p 10.5.ceil
+
+p 3.14159.round(4)
+p 3.6.round
+
+p(-35.67.abs)
