f5022579f8163320afa3553b29e8cfed7a02804c
James McCarron <mccarron.j@gmail.com>
2021-03-18 13:33:23 +1100

    .upto .downto Methods

--- Ruby_Basics/assignment_operators.rb
+++ Ruby_Basics/assignment_operators.rb
@@ -14,4 +14,22 @@ b -= 50
 p b
 
 # Introduction to Blocks
-# 3.times { puts 'James is awesome!' }
+# 3.times { |count| puts 'We are on number #{count}' }
+
+# 3.times do |i|
+#   puts "We are currently on loop number #{i}"
+#   puts 'James is the MAN!'
+#   puts "I'm having so much fun learning Ruby!!"
+# end
+
+# Use the times method to output the first
+# ten multiples of 3 (3, 6, 9, 12 .. 30)
+
+10.times do |count|
+  puts "Alright, let's show the next multiple!"
+  puts (3 * (count + 1)).to_s
+end
+
+# OR
+
+10.times { |count| puts (3 * (count + 1)).to_s }
--- Ruby_Basics/methods_variables.rb
+++ Ruby_Basics/methods_variables.rb
@@ -1,1 +1,21 @@
+# frozen_string_literal: true
+
 puts 'Hello World'
+
+# .upto and .downto Methods
+
+5.downto(1) { |i| puts "Countdown: #{i}" }
+
+5.downto(0) do |current_number|
+  puts "We are currently on #{current_number}"
+  puts 'Hooray!'
+end
+
+puts 'LIFTOFF!!'
+
+5.upto(10) do |num|
+  puts "We're moving on up!"
+  puts "We are currently on #{num}"
+end
+
+puts 'BAZINGA!!'
