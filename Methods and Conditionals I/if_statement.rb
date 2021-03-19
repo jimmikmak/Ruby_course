66b6b0bc377fe04ff7797e46e2ecc9dc1134f5e5
James McCarron <mccarron.j@gmail.com>
2021-03-19 13:59:50 +1100

    Method Parameters

--- Methods and Conditionals I/if_statement.rb
+++ Methods and Conditionals I/if_statement.rb
@@ -1,1 +1,35 @@
 # frozen_string_literal: true
+
+# Intro to Methods
+
+def introduce_myself
+  puts 'I am handsome'
+  puts 'I am talented'
+  puts 'I am brilliant'
+end
+
+introduce_myself
+
+# Local Variables
+
+expression = 'I am handsome!!'
+
+def introduce_myself
+  expression = 'I am a genius!!'
+  puts expression
+end
+
+introduce_myself
+puts expression
+
+# Parameters and Arguments
+
+def praise_person(name, age)
+  puts "#{name} is amazing"
+  puts "#{name} is charming"
+  puts "#{name} is talented"
+  puts "His age is #{age}"
+end
+
+praise_person('James', 42)
+praise_person('Clem', 41)
