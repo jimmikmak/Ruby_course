a25a723b60296bf5369b76cb2bd3e57647a39fcb
James McCarron <mccarron.j@gmail.com>
2021-03-22 17:07:29 +1100

    .respond_to? Method

---
+++ Methods and Conditionals I/nested_if_statements.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+# Parentheses precedence
+
+def authenticate_agent(rank, name, credentials)
+  if (rank == '007' && name == 'James Bond') || credentials == 'Secret Agent'
+    puts 'ACCESS GRANTED, please proceed to Sector 7.'
+  else
+    puts "ACCESS DENIED, #{name}"
+  end
+end
+
+authenticate_agent('007', 'James Bond', 'Spy')
+
+# Nested if statements (often not 'best practice')
+
+def meal_plan(time_of_week, time_of_day)
+  if time_of_week == 'weekday'
+    if time_of_day == 'breakfast'
+      'Cereal'
+    elsif time_of_day == 'lunch'
+      'Sandwich'
+    elsif time_of_day == 'dinner'
+      'Chicken Nuggets'
+    end
+  elsif time_of_week == 'weekend'
+    if time_of_day == 'breakfast'
+      'French Toast'
+    elsif time_of_day == 'lunch'
+      'BBQ Chicken Pizza'
+    elsif time_of_day == 'dinner'
+      'Steak'
+    end
+  end
+end
+
+p meal_plan("weekday", "lunch")
+p meal_plan("weekday", "dinner")
+p meal_plan("weekend", "breakfast")
+p meal_plan("weekend", "dinner")
+
+# 
