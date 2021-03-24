a19e3fb5f093d26b46715e663586c7b773122de0
James McCarron <mccarron.j@gmail.com>
2021-03-24 16:52:34 +1100

    unless_while_until Keywords

--- Methods and Conditionals II/negation_and_keywords.rb
+++ Methods and Conditionals II/negation_and_keywords.rb
@@ -8,3 +8,22 @@
 # end
 
 puts !1
+p !!''
+p !!1
+p !!3.14
+
+p !!false
+p !!nil
+
+# The unless Keyword
+password = 'dominoes'
+
+if password == 'whiskers'
+  puts "That's the correct password, welcome!"
+else
+  puts 'Not allowed'
+end
+
+# password == 'dominoes'
+
+# puts 'It does not include the letter.' unless password.include?('a')
